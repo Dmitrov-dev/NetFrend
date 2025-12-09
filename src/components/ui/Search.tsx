9f60bdaf6914ddcc7a6e1cfe674de826a0c6661a
Dmitrov <mityusha.efrem@gmail.com>
2025-12-10 00:28:29 +0300

    09

--- src/components/ui/Search.tsx
+++ src/components/ui/Search.tsx
@@ -14,7 +14,7 @@ export function Search({ searchTerm, setSearchTerm }: SearchProps) {
       value={searchTerm}
       onChange={(e) => setSearchTerm(e.target.value)}
       placeholder="Search..."
-      className={`border px-2 py-1 rounded outline-0 {
+      className={`border px-2 py-1 rounded outline-0 ${
         theme === 'dark'
           ? 'bg-black text-white border-white/15'
           : 'bg-white text-black border-black/15'
